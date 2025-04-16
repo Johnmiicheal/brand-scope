5ae81754dd455429dc18a15e88f4ce9f4cbec2a3
johnmiicheal <michealelijah301@gmail.com>
2025-04-16 06:44:07 +0100

    vercel maxDuration error --companymap

--- src/app/api/companymap/route.ts
+++ src/app/api/companymap/route.ts
@@ -4,7 +4,7 @@ import { generateObject } from 'ai';
 import { z } from 'zod';
 import { groq } from '@ai-sdk/groq';
 
-export const maxDuration = 100;
+export const maxDuration = 60;
 
 export async function POST(req: NextRequest) {
   try {
